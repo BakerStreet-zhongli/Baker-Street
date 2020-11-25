0f6f703fefa4cfb0e96010408fa4d57c129dc8d7
chanchanlee <cavalry51032@gmail.com>
2020-11-25 16:41:33 +0800

    add customer page
    new

---
+++ wait/cdbinit.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAS7GqRdNRvDiWw0pBwzyQjxEAdqyKewEU",
+    authDomain: "research-8ca6f.firebaseapp.com",
+    databaseURL: "https://research-8ca6f.firebaseio.com",
+    storageBucket: "research-8ca6f.appspot.com",
+    messagingSenderId: "387566171434"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
